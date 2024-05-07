287c8ac19b26fbc1ffa0a3523eb999b49b9833f7
Asahi <c0b23004b6@edu.teu.ac.jp>
2024-05-07 14:50:10 +0900

    練習２

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -162,10 +162,16 @@ def main():
             pg.display.update()
             time.sleep(1)
             return
+        if beam is not None and bomb is not None:
+            if beam.rct.colliderect(bomb.rct):
+                beam = None
+                bomb = None
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         bomb.update(screen)
+        if bomb is not None:
+            bomb.update(screen)
         if beam is not None:
             beam.update(screen)
         pg.display.update()
